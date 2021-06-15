9aa82e14e409619c11714a1636f20925159ce8ab
Brenda Kiptim <kiptim54>
2021-06-15 13:58:02 +0300

    changed name

---
+++ pest_control_frontend/src/modules/authentication/Login.tsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+const Login = (): JSX.Element => {
+  return <div>This is the login page</div>
+}
---
+++ pest_control_frontend/src/modules/authentication/SignUp.tsx
@@ -0,0 +1,6 @@
+import React from "react"
+
+const SignUp = () => {
+  return <div>This is the signUp</div>
+}
+export default SignUp
